0848124161e57524d0925ca6a92b3a318ef2b42b
dkstools <120433478+dkstools@users.noreply.github.com>
2022-12-12 18:53:24 -0800

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,5 +1,35 @@
-'use strict';
-
-const app = require('./express/server');
-
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+{
+  "name": "node-js-getting-started",
+  "version": "0.3.0",
+  "description": "A sample Node.js app using Express 4",
+  "engines": {
+    "node": "16.x"
+  },
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "node test.js"
+  },
+  "dependencies": {
+    "body-parser": "^1.20.0",
+    "ejs": "^3.1.5",
+    "express": "^4.18.1",
+    "ip": "^1.1.8",
+    "multer": "^1.4.5-lts.1",
+    "node-telegram-bot-api": "^0.57.0"
+  },
+  "devDependencies": {
+    "got": "^11.3.0",
+    "tape": "^4.7.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/heroku/node-js-getting-started"
+  },
+  "keywords": [
+    "node",
+    "heroku",
+    "express"
+  ],
+  "license": "MIT"
+}
